20a41ac913c7324119c80628be94ed6409d6f67c
A. Scott Tribble <a.scott.tribble@gmail.com>
2018-01-10 22:18:59 -0800

    added mlab url to database_url

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 'use strict';
 
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/test-blog-app';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/test-blog-app' || 'mongodb://dev:dev@ds251277.mlab.com:51277/blog-app-scott';
 exports.PORT = process.env.PORT || 8080;
